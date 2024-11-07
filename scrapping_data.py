cf81411dfd7d87759fc45e1013dd0c1c0d471648
Shakhobiddin Bozorov <gravitifolz.01@gmail.com>
2024-11-07 08:07:58 +0500

    updates

--- scrapping_data.py
+++ scrapping_data.py
@@ -1,10 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
+from json_actions import load_data, save_data
+from variables import city_data
+import schedule
+import time
 
-response = requests.get('https://namozvaqti.uz/shahar/toshkent')
+user_info = load_data('Json/user_info.json')
 
-soup = BeautifulSoup(response.content, 'html.parser')
+def update_user_info():
+    global user_info
+    user_info = load_data('Json/user_info.json')
 
-link = str(soup.find_all('script')[-1])
+def fetch_and_save_data():
+    data = {}
+    for i in user_info:
+        response = requests.get(f"https://namozvaqti.uz/shahar/{user_info[i]['city'].lower()}")
+        soup = BeautifulSoup(response.content, 'html.parser')
+        link = str(soup.find_all('script')[-1])
+        data[i] = link[link.find('const times')+25 : link.find('const times')+30]
+    save_data('Json/times.json', data)
 
-print(link[link.find('const times')+25 : link.find('const times')+30])
+schedule.every().day.at("00:00").do(fetch_and_save_data)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
